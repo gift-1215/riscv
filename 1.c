d9991751154f4479764f66d0e035c3b9d61cab8f
gift-1215 <62284352+gift-1215@users.noreply.github.com>
2023-04-03 04:23:08 +0800

    init_1.c

--- 1.c
+++ 1.c
@@ -1,4 +1,13 @@
-#include<stdio.h>
-int main(){
+// description: a = a / b
+#include <stdio.h>
+int main ()
+{
+	int a, b;
+	FILE *input = fopen("../input/1.txt","r");
+	fscanf(input, "%d %d", &a, &b);
+	fclose(input);
+	//a = a / b;
+	asm volatile(/*Your Code*/);
+	printf("%d\n", a);
 	return 0;
 }
