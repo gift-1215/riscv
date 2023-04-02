d382ccb0d0ae420abef4d0033e63123b2e9b8b24
gift-1215 <wulee1215@gmail.com>
2023-04-02 23:05:42 +0800

    4_3

--- 1.c
+++ 1.c
@@ -7,7 +7,19 @@ int main ()
 	fscanf(input, "%d %d", &a, &b);
 	fclose(input);
 	//a = a / b;
-	asm volatile(/*Your Code*/);
+	asm volatile(
+		"mov %[A], %%eax\n\t"
+		"mov %[B], %%ebx\n\t"
+		"xor %%edx, %%edx\n\t"
+
+		"idiv %%ebx\n\t"
+		"mov %%eax, %[A]\n\t"
+
+		:[A] "+r"(a)
+		:[B] "r"(b)
+		:"eax","ebx","edx"
+			
+	);
 	printf("%d\n", a);
 	return 0;
 }
